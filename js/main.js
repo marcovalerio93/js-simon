44578aad58dd1014de21ab43d72a5d322ba6eb21
Marco Valerio <mo.valerio93@gmal.com>
2022-07-19 08:59:03 +0200

    js simon structure

---
+++ js/main.js
@@ -0,0 +1,27 @@
+/*
+1 - array numeri casuali
+2 - collego numeri html
+3 - mostro i numeri per 30 secondi
+4 - inserisco numeri
+5 - verifico se i numeri inseriti sono inseriti 
+6 - stampo risultati
+ */
+
+const numRandom = [];
+
+
+
+
+
+
+
+
+
+
+
+
+/*funzioni */
+
+function randomNum (min, max) {
+    return Math.floor(Math.random() * (max - min +1)) +min;
+}
