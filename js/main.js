31bb8639128dc2825e835a60fc62931eb0bf5dc1
Marco Valerio <mo.valerio93@gmal.com>
2022-07-19 14:59:31 +0200

    js simon done

--- js/main.js
+++ js/main.js
@@ -7,6 +7,8 @@
 6 - stampo risultati
  */
 
+
+//1
 const numRandom = [];
 
 while (numRandom.length < 5) {
@@ -16,7 +18,16 @@ while (numRandom.length < 5) {
     }
  }
 
-document.getElementById('display').innerHTML = numRandom;
+const displayDom = document.getElementById('display').innerHTML = numRandom;
+const textDom = document.getElementById('text')
+
+displayDom.innerHTML = numRandom;
+
+2//
+
+setTimeout(timeDom, 30000);
+
+setTimeout(numInput, 31000);
 
 
 
@@ -29,3 +40,25 @@ function randomNum(min, max) {
 
     return Math.floor(Math.random() * (max - min + 1)) + min;
 } 
+
+function timeDom() {
+    displayDom.innerHtml = '';
+    textDom.innerHTML = 'Quali erano i 5 numeri?'
+} 
+
+function prova(){
+    const prova= prompt('prova');
+     
+}
+
+function numInput() {
+    const num = [];
+    while (num.length < 5) {
+        const numInput = prompt('inserisci numeri');
+        if (! num.includes(numInput)) {
+            num.push(numInput);
+        }
+    }
+    console.log(num);
+
+}
